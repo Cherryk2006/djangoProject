d5207fcc8c31bc587d08d69e6b176d25e04374a1
Cherryk2006 <anastasya.cherednyk@gmail.com>
2025-01-14 17:17:58 -0600

    New commit

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 import booking
 import users.views
 import booking.views
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -14,12 +15,36 @@ def specific_user(request, user_id):
 
 
 def login_page(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == "Get":
+        return render(request, "login.html")
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("You are now logged in.")
+        else:
+            return render(request, "login.html")
 
 
 def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return HttpResponse("You are now logged out.")
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def register_page(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == "Get":
+        return render(request, "register.html")
+    else:
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+
+        user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+        user.save()
+        return HttpResponse("Hello, world. You're at the polls index.")
